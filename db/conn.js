88cd9a4ca685c47189928fa13355a91972695e81
Gustavo <gustavosoaresok@gmail.com>
2025-06-10 20:04:48 -0300

    Add PostgreSQL

--- db/conn.js
+++ db/conn.js
@@ -1,26 +1,30 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-
-const sequelize = new Sequelize('eventos2','root','root',{
-
-    host:'localhost',
-    dialect:'mysql',
-})
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 
 try {
-    sequelize.authenticate()
-    console.log('conectamos')
+  sequelize.authenticate();
+  console.log('Conectado ao banco de dados PostgreSQL com sucesso!');
 } catch (error) {
-    console.log(`não conectamos:${err}`)
+  console.log(`Erro ao conectar ao banco de dados: ${error}`);
 }
 
-
 sequelize.sync({ alter: true })
-    .then(() => {
-        console.log('Banco de dados atualizado com sucesso!')
-    })
-    .catch((error) => {
-        console.error('Erro ao atualizar o banco de dados:', error)
-    })
+  .then(() => {
+    console.log('Banco de dados atualizado com sucesso!');
+  })
+  .catch((error) => {
+    console.error('Erro ao atualizar o banco de dados:', error);
+  });
 
-module.exports = sequelize
+module.exports = sequelize;
